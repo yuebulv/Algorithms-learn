68641fc195ce21c642af801061474448deab7cb8
yuebu_global <yuebu_global@qq.com>
2020-04-19 01:16:01 +0800

    add rename dir

--- isnot.py
+++ isnot.py
@@ -1,24 +1,8 @@
-import os
-import numpy as np
-"""
-    #从filename文件中恢复文件名
-"""
-def renamedir(path):
-    files = os.listdir(path)
-    filenumber=1
-    for file in files:
-        if os.path.isdir(path + '/' + file):
-            # renamedir(path + '/' + file)
-            # filenewname = str(filenumber) + "rename"
-            # while os.path.exists(path + '/' + filenewname): #判断是否已存在文件名
-            #     filenumber += 1
-            #     filenewname=str(filenumber)+"rename"
-            # os.rename(path + '/' + file, path + '/' + filenewname)
-            # with open(path + '/' + 'filename.txt', 'a') as f:#记录文件名
-            #     f.write(file+'\t'+filenewname+"\n")
-            #     f.close()
-            # filenumber += 1
+import tkinter as tk
+from tkinter import filedialog
 
-list_a = []
-path = "F:\\2020-RoadProject\GuiZhou-province\ChishuiCountryroad\\3-hint"
-contents = renamedir(path)
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askdirectory()
+print(file_path)
--- renameback.py
+++ renameback.py
@@ -6,10 +6,28 @@ import numpy as np
 def renameback(path):
     files = os.listdir(path)
     readestatus = 0
+    dirreadstatus=0
     if os.path.exists(path + '/' + 'filename.txt'):
         for file in files:
             if os.path.isdir(path + '/' + file):
                 renameback(path + '/' + file)
+                if dirreadstatus == 0:
+                    with open(path + '/' + 'dirname.txt', 'r') as f:  # 读入filename文件名
+                        line = f.readline()
+                        data_list = []
+                        while line:
+                            line = line.strip('\n')
+                            num = list(line.split('\t'))
+                            data_list.append(num)
+                            line = f.readline()
+                        f.close()
+                        all_dirs = np.array(data_list)
+                        lineofall_dirs = len(all_dirs)
+                        dirreadstatus=1
+                for i in range(lineofall_dirs):
+                    if file==all_dirs[i,1]:
+                        os.rename(path + '/' + file, path + '/' + all_dirs[i,0])
+                        break
             elif os.path.isfile(path + '/' + file) and (file.find('filename.txt')<0):
                 if readestatus == 0:
                     with open(path + '/' + 'filename.txt', 'r') as f:  # 读入filename文件名
@@ -31,6 +49,13 @@ def renameback(path):
         readestatus = 0
         return all_files
     # return all_files
-list_a = []
+
+
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
 path = "F:\\2020-RoadProject\GuiZhou-province\ChishuiCountryroad\\3-hint"
-contents = renameback(path)
+file_path = filedialog.askdirectory()
+contents = renameback(file_path)
